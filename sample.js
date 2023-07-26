1d2e281363d8e0f3f6ce3b36640730dd1e54b3fa
김경곤 <kg1942@invesume.com>
2023-07-26 12:10:56 +0900

    test: add build

--- sample.js
+++ sample.js
@@ -7,3 +7,4 @@ console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
+console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
